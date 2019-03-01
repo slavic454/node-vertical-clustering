5e633294efdf2b9d79110e5ac2a18d80a1fe8b3c
Karol Bilicki <karolbilicki@gmail.com>
2019-03-01 21:16:39 +0100

    Add simple cluster

---
+++ index.js
@@ -0,0 +1,30 @@
+var cluster = require('cluster');
+var os = require('os');
+var express = require('express');
+
+if(cluster.isMaster) {
+    var numWorkers = os.cpus().length;
+
+    console.log('Master cluster setting up ' + numWorkers + ' workers...');
+
+    for(var i = 0; i < numWorkers; i++) {
+        cluster.fork();
+    }
+
+    cluster.on('online', function(worker) {
+        console.log('Worker ' + worker.process.pid + ' is online');
+    });
+
+    cluster.on('exit', function(worker, code, signal) {
+        console.log('Worker ' + worker.process.pid + ' died with code: ' + code + ', and signal: ' + signal);
+        console.log('Starting a new worker');
+        cluster.fork();
+    });
+} else {
+    var app = express();
+    app.all('/*', function(req, res) {res.send('process ' + process.pid + ' says hello!').end();})
+
+    var server = app.listen(8000, function() {
+        console.log('Process ' + process.pid + ' is listening to all incoming requests');
+    });
+}
